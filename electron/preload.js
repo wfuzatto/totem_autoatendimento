22f170eb571b733e3175884217c897f408f47f72
wfuzatto <shockwave_acb@hotmail.com>
2026-08-23 13:31:28 -0300

    feat(v2): bridge virtual keyboard input to Electron

--- electron/preload.js
+++ electron/preload.js
@@ -2,5 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('kiosk', {
   exit: () => ipcRenderer.send('kiosk-exit'),
-  setZoomFactor: factor => ipcRenderer.send('kiosk-zoom', factor)
+  setZoomFactor: factor => ipcRenderer.send('kiosk-zoom', factor),
+  sendVirtualKey: payload => ipcRenderer.send('kiosk-keyboard-input', payload),
+  isElectron: true
 });
