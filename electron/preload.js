97eed8340d98598a3143ff951be30ce22027b250
wfuzatto <shockwave_acb@hotmail.com>
2026-08-23 16:23:40 -0300

    fix(v2): remember focused iframe for virtual keyboard

--- electron/preload.js
+++ electron/preload.js
@@ -3,6 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('kiosk', {
   exit: () => ipcRenderer.send('kiosk-exit'),
   setZoomFactor: factor => ipcRenderer.send('kiosk-zoom', factor),
+  rememberVirtualKeyboardTarget: () => ipcRenderer.send('kiosk-keyboard-remember-frame'),
   sendVirtualKey: payload => ipcRenderer.send('kiosk-keyboard-input', payload),
   isElectron: true
 });
