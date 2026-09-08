af2c2f9cc9cfeb3aeea7d42cca55fd09e3b377c8
wfuzatto <shockwave_acb@hotmail.com>
2026-09-08 01:37:28 -0300

    feat: install Face Scanner integration runtime

--- src/server-main.js
+++ src/server-main.js
@@ -4,9 +4,11 @@ const https = require('https');
 const app = require('./server-runtime');
 const { installCheckoutRuntime } = require('./checkout-runtime');
 const { installDocumentRemovalRuntime } = require('./document-removal-runtime');
+const { installFaceScannerRuntime } = require('./face-scanner-runtime');
 
 installCheckoutRuntime(app);
 installDocumentRemovalRuntime(app);
+installFaceScannerRuntime(app);
 
 function start() {
   const port = Number(process.env.PORT || 3080);
